41cba45c0e38e8a08e37da46b6857e9b24a54a01
Jorge <jorge@Jorges-MacBook-Pro.local>
2019-04-09 22:44:22 -0400

    Finished front and back, next users or database

--- public/javascripts/movies.js
+++ public/javascripts/movies.js
@@ -1,35 +1,69 @@
 $(function() {
 
-  var App = {
+  const FrontPage = {
 
     movies: {},
-    latestStartEnd: [],
+    position: {'latest': {'start': 0, 'end': 4}, 'topRated': {'start': 0, 'end': 4}, 'trending': {'start': 0, 'end': 4}},
 
-    displayLatest: function() {
-      console.log('displayLatest');
+    displayAllLists: function() {
+      console.log('displayAllLists')
+      var movieLists = ['latest', 'topRated', 'trending']
+      movieLists.forEach((movieList)=>{
+        this.displayList(movieList)
+      })
+    },
+    displayList: function(movieList) {
+      console.log('displayList');
       var tempFunc = Handlebars.compile($('#mListing').html());
-      this.latestStartEnd = [0,4]
-      var preview = this.movies.latest.slice(this.latestStartEnd[0], this.latestStartEnd[1])
-      $('#latest').html(tempFunc({movies: preview}))
+      var preview = this.movies[movieList].slice(this.position[movieList].start, this.position[movieList].end)
+      $(`#${movieList}`).html(tempFunc({movies: preview}))
+      this.formatDates(movieList)
     },
 
-    displayTrending: function() {
-      console.log('displayTrending');
-      var tempFunc = Handlebars.compile($('#mListing').html());
-      this.trendingStartEnd = [0,4]
-      var preview = this.movies.trending.slice(this.trendingStartEnd[0], this.trendingStartEnd[1])
-      $('#trending').html(tempFunc({movies: preview}))
+    showDescripton: function() {
+      console.log('showDescripton');
+      $('main').on('mouseenter', 'li', (e)=>{
+        $(e.target).closest('li').find('div').css('opacity', '1');
+      }).on('mouseleave', 'div', (e)=>{
+        $(e.target).css('opacity', '0');
+      }) 
     },
 
-    displayTopRated: function() {
-      console.log('displayTopRated');
-      var tempFunc = Handlebars.compile($('#mListing').html());
-      this.topRatedStartEnd = [0,4]
-      var preview = this.movies.topRated.slice(this.topRatedStartEnd[0], this.topRatedStartEnd[1])
-      $('#topRated').html(tempFunc({movies: preview}))
+    formatDates: function(movieList) {
+      $(`#${movieList} .date`).each((idx, el)=>{
+        $(el).text(`(${$(el).text().substring(0,4)})`)
+      })
     },
 
-    retrieveMovies: function() {
+    nextSlide: function() {
+      console.log('nextSlide');
+      $(`i[class='fas fa-chevron-right fa-3x'`).on('click', (e)=>{        
+        var movieList = $(e.target.previousElementSibling).attr('id');
+        console.log('name', movieList);
+        if(this.position[movieList].end === 20) {          
+        } else {
+          this.position[movieList].start +=4
+          this.position[movieList].end +=4
+          this.displayList(movieList)
+        }
+      })
+    },
+
+    previousSlide: function() {
+      console.log('previousSlide');
+      $(`i[class='fas fa-chevron-left fa-3x'`).on('click', (e)=>{
+        var movieList = $(e.target.nextElementSibling).attr('id');
+        console.log('name', movieList);
+        if(this.position[movieList].start === 0) {
+        } else {          
+          this.position[movieList].start -=4
+          this.position[movieList].end -=4
+          this.displayList(movieList)
+        }
+      })
+    },
+
+    loadFrontPage: function() {
       const request = new XMLHttpRequest();
       request.open('GET', 'http://localhost:3000/movies')
       request.responseType = 'json';
@@ -38,9 +72,10 @@ $(function() {
         if (request.status === 200) {
           this.movies = request.response 
           console.log('clientside', this.movies)
-          this.displayLatest();
-          this.displayTrending();
-          this.displayTopRated();
+          this.displayAllLists();
+          this.nextSlide();
+          this.previousSlide();
+          this.showDescripton();
         } else {
           console.log(request.statusText)
         } 
@@ -48,6 +83,90 @@ $(function() {
     },
   }  
 
-  App.retrieveMovies()
+  const NavPage = {
+
+    searchMovie: function() {
+      $('form').on('submit', (e)=>{
+        e.preventDefault();
+        var query = $('input').val();
+        console.log('searchMovie!')
+        const request = new XMLHttpRequest();
+        const path = `http://localhost:3000/results/${query}`;
+        request.open('GET', path);
+        request.send()
+        request.addEventListener('load', ()=>{
+          if (request.status === 200) {
+            console.log('client', query);
+            $('main').attr('id', 'back');
+            $('main').html(request.response);
+          }
+        })
+      })
+    },
+
+
+    loadNavPage: function() {
+      this.searchMovie();
+    },
+
+  }
+
+  const BackPage = {
+  
+    loadBackPage: function() {
+      this.addMovie()
+      this.deleteMovie()
+    },
+
+    addMovie: function() {
+      console.log('addItem')
+      $('main').on('click', 'button.addMovie', (e)=>{
+        var id = $(e.target).attr('value');
+        console.log('addMovie inside', id);
+        this.addMovieToList(id);
+      })
+    },
+  
+    addMovieToList: function(id) {
+      console.log('addMovieTolist')
+      const request = new XMLHttpRequest();
+      request.open('POST', `http://localhost:3000/add/${id}`)
+      request.send()
+      request.addEventListener('load', ()=>{
+        if (request.status === 200) {
+          $('#yourList').html(request.response);
+        }
+      })
+    },
+
+    deleteMovie: function() {
+      console.log('deleteMovie')
+      $('main').on('click', 'button.deleteMovie', (e)=>{
+        var id = $(e.target).attr('value');
+        console.log('deleteMovie inside', id);
+        this.deleteMovieFromList(id);
+      })
+    },
+
+    deleteMovieFromList: function(id) {
+      console.log('deleteMovieFromList', id)
+      const request = new XMLHttpRequest();
+      request.open('DELETE', `http://localhost:3000/delete/${id}`)
+      request.send()
+      request.addEventListener('load', ()=>{
+        if (request.status === 200) {
+          $('#yourList').html(request.response);
+        }
+      })
+    },
+
+  }
+
+
+  
+
+  NavPage.loadNavPage()
+  FrontPage.loadFrontPage()
+  BackPage.loadBackPage()
 
 })
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request')
+var path = require('path');
+
+var List = require(path.resolve(path.dirname(__dirname), './modules/list.js'))
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('get / request')
   res.render('index', {
-    title: 'Hello World',
+    title: 'movieGen',
   });
 });
 
 router.get('/movies', (req, res, next)=>{
   console.log('get /movies request')
-  var movies = {};
   new Promise((resolve, reject)=>{
+    var movies = {};
     var encodedPath = 'https://api.themoviedb.org/3/trending/movie/week?api_key=372fa67260808fa002d6f3471d8dc7b3'
     request(encodedPath, (error, response, body)=>{
       var rawBody =JSON.parse(body);
@@ -21,16 +24,17 @@ router.get('/movies', (req, res, next)=>{
       console.log('trending', movies.trending.length);
       resolve(movies);
     })
-  }).then(()=>{
+  }).then((movies)=>{
     console.log('latest Start')
     var encodedPath = 'https://api.themoviedb.org/3/discover/movie?api_key=372fa67260808fa002d6f3471d8dc7b3&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&primary_release_date.lte=2019-04-06&vote_count.gte=500'
-    request(encodedPath, (error, response, body)=>{
+    return new Promise((resolve, reject)=>{request(encodedPath, (error, response, body)=>{
       var rawBody = JSON.parse(body);
       movies.latest = rawBody.results
       console.log('latest', movies.latest.length)
+      resolve(movies);
+      })
     })
-    return movies;
-  }).then(()=>{
+  }).then((movies)=>{
     console.log('topRated Start')
     var encodedPath = 'https://api.themoviedb.org/3/discover/movie?api_key=372fa67260808fa002d6f3471d8dc7b3&language=en&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&vote_count.gte=500&vote_average.gte=7'
     request(encodedPath, (error, response, body)=>{
@@ -42,19 +46,38 @@ router.get('/movies', (req, res, next)=>{
   })
 });
 
+router.get('/results/:query', function(req, res, next) {
+  console.log('GET results query')
+  var encodedQuery = encodeURIComponent(req.params.query); 
+  var encodedPath = `https://api.themoviedb.org/3/search/movie?api_key=372fa67260808fa002d6f3471d8dc7b3&query=${encodedQuery}&page=1`
+  request(encodedPath, (error, response, body)=>{
+    var rawBody =JSON.parse(body);
+    console.log('request to tmdb', rawBody.total_results)
+    List.results = rawBody.results;
+    res.render('results', {
+      searchTerm: req.params.query,
+      searchList: rawBody.results,
+      savedList: List.retrieveSavedMovies(),
+    })
+  })
+});
 
+router.post('/add/:id', (req, res, next)=>{
+  console.log('POST add', req.params.id)
+  List.addMovieById(req.params.id);
+  console.log('movie List', List.retrieveSavedMovies());
+  res.render('yourList', {
+    savedList: List.retrieveSavedMovies(),
+  })
+})
 
-// router.get('/results', function(req, res, next) {
-  // var encodedQuery = 'shadowlands'; 
-  // var encodedPath = `https://api.themoviedb.org/3/search/movie?api_key=372fa67260808fa002d6f3471d8dc7b3&query=${encodedQuery}&page=1`
-  // request(encodedPath, (error, response, body)=>{
-  //   var rawBody =JSON.parse(body);
-  //   res.render('index', {
-  //     movieArr: rawBody.results,
-  //   });
-  // })
-// });
-
+router.delete('/delete/:id', (req, res, next)=>{
+  console.log('DELETE movie', req.params.id)
+  List.removeMovieById(req.params.id)
+  res.render('yourList', {
+    savedList: List.retrieveSavedMovies(),
+  })
+})
 
 
 
