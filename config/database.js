1da45a32750bc692cc1d613515005fc03e4bdcf7
Jorge <jorge@Jorges-MacBook-Pro.local>
2019-05-07 23:42:33 -0400

    sequelize local

--- config/database.js
+++ config/database.js
@@ -1,17 +1,17 @@
 const Sequelize = require('sequelize');
 
 // Option 1 (localhost)
-// module.exports = new Sequelize("movie_collection", null, null, {
-//   host: 'localhost',
-//   dialect: 'postgres',
+module.exports = new Sequelize("movie_collection", null, null, {
+  host: 'localhost',
+  dialect: 'postgres',
 
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   }
-// })
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  }
+})
 
 
 
