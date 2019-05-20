c133b2be9b9746b1ecab803d5a59a64e575a78d9
floresjmjr <floresjmjr@gmail.com>
2019-05-20 13:24:15 -0400

    env var data fix

--- config/database.js
+++ config/database.js
@@ -1,29 +1,13 @@
 const Sequelize = require('sequelize');
 
 
-if (process.env.DB_URL) {
-  module.exports = new Sequelize(process.env.DB_URL, {
-    
-    define: {
-      timestamps: false,
-    },
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+  
+  define: {
+    timestamps: false,
+  },
 
-  });
-} else {
-  module.exports = new Sequelize("movie_collection", null, null, {
-    host: 'localhost',
-    dialect: 'postgres',
+});
 
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-    define: {
-      timestamps: false,
-    },
-  })
-}
 
 
